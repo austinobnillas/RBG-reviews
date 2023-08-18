b07d49a5e97adb8c2bccf226f4c30e4ff1ccb53f
Rheanne Bande <rheanne.bande@gmail.com>
2023-08-18 12:56:17 -0700

    Create homepage boilerplate

---
+++ client/src/components/HomePage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import {Link, useNavigate} from "react-router-dom";
+import axios from "axios";
+
+const Home = (props) => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Home;

91d2867675117f12506566d40d9e145678661582
Ongjunco-J <00mojojojo29@gmail.com>
2023-08-21 01:53:05 -0700

    minor spelling error

--- client/src/components/login.jsx
+++ client/src/components/login.jsx
@@ -1,5 +1,5 @@
 import React, {useState} from "react";
-import '.login.css';
+import '.Login.css';
 import axios from 'axios'
 
 const Login = () => {
--- client/src/components/register.jsx
+++ client/src/components/register.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from  'react';
-import ('.register.css')
+import ('.Register.css')
 import axios from 'axios'
     
 const UserForm = (props) => {
